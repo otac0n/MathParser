57674c16c37971766f09d9e77f508ba5e3861a4b
John Gietzen <john@gietzen.us>
2025-03-22 14:15:45 -0700

    Moved parser members into a partial file.

---
+++ MathParser/Parser.cs
@@ -0,0 +1,95 @@
+﻿namespace MathParser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Numerics;
+    using System.Reflection;
+    using Pegasus.Common;
+
+    public partial class Parser
+    {
+        private static Expression ConvertIfLower(Expression expression, Expression to)
+        {
+            return ConvertIfLower(expression, to: to.Type);
+        }
+
+        private static Expression ConvertIfLower(Expression expression, Type to)
+        {
+            var from = expression.Type;
+            if (to == typeof(Complex) && from != typeof(Complex))
+            {
+                return Expression.Convert(expression, to);
+            }
+
+            return expression;
+        }
+
+        private static Expression Ceiling(Expression expression)
+        {
+            return Expression.Call(typeof(Math).GetMethod(nameof(Math.Ceiling), new[] { expression.Type }), expression);
+        }
+
+        private static Expression Floor(Expression expression)
+        {
+            return Expression.Call(typeof(Math).GetMethod(nameof(Math.Floor), new[] { expression.Type }), expression);
+        }
+
+        private static Expression Pow(Expression @base, Expression exponent)
+        {
+            @base = ConvertIfLower(@base, to: typeof(Complex));
+            return Expression.Call(typeof(Complex).GetMethod(nameof(Complex.Pow), new[] { @base.Type, exponent.Type }), @base, exponent);
+        }
+
+        private static Expression Sqrt(Expression @base)
+        {
+            @base = ConvertIfLower(@base, to: typeof(Complex));
+            return Expression.Call(typeof(Complex).GetMethod(nameof(Complex.Sqrt), new[] { @base.Type }), @base);
+        }
+
+        private static Expression Function(string name, IList<Expression> arguments)
+        {
+            Expression[] mappedArguments;
+            var found = FindFunction(typeof(Complex), name, arguments.Select(a => a.Type).ToArray());
+            if (found == null)
+            {
+                found = FindFunction(typeof(Complex), name, arguments.Select(_ => typeof(Complex)).ToArray());
+                if (found == null)
+                {
+                    throw new MissingMethodException(typeof(Complex).FullName, name + "(" + string.Join(", ", arguments.Select(a => a.Type.FullName)) + ")");
+                }
+                else
+                {
+                    mappedArguments = arguments.Select(a => ConvertIfLower(a, to: typeof(Complex))).ToArray();
+                }
+            }
+            else
+            {
+                mappedArguments = arguments.ToArray();
+            }
+
+            return Expression.Call(found, mappedArguments);
+        }
+
+        private static MethodInfo FindFunction(Type type, string name, Type[] argTypes)
+        {
+            return (from m in type.GetMethods(BindingFlags.Public | BindingFlags.Static)
+                    where m.DeclaringType == type
+                    where m.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase)
+                    let parameters = m.GetParameters()
+                    where parameters.Length == argTypes.Length
+                    where Enumerable.Range(0, argTypes.Length).All(i => parameters[i].ParameterType.IsAssignableFrom(argTypes[i]))
+                    select m).FirstOrDefault();
+        }
+
+        private static void CreateVariable(Cursor state, string variable)
+        {
+            var existing = state[variable] as Expression;
+            if (existing == null)
+            {
+                state[variable] = Expression.Parameter(typeof(Complex), variable);
+            }
+        }
+    }
+}
