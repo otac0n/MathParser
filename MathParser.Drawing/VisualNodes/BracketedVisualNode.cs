c0b9e533753ea1b4aac843d4813e14d0083ad26a
John Gietzen <john@gietzen.us>
2025-03-29 02:18:25 -0700

    Improved brackets across all fonts by sizing using the path extrema.

--- MathParser.Drawing/VisualNodes/BracketedVisualNode.cs
+++ MathParser.Drawing/VisualNodes/BracketedVisualNode.cs
@@ -4,14 +4,21 @@ namespace MathParser.Drawing.VisualNodes
 {
     using System;
     using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Runtime.Versioning;
 
+    [SupportedOSPlatform("windows")]
     internal class BracketedVisualNode : VisualNode
     {
+        private readonly string bothBrackets;
+
         public BracketedVisualNode(string leftBracket, VisualNode node, string rightBracket)
         {
             this.LeftBracket = leftBracket;
             this.Node = node;
             this.RightBracket = rightBracket;
+
+            this.bothBrackets = leftBracket + rightBracket;
         }
 
         public string LeftBracket { get; }
@@ -28,11 +35,12 @@ namespace MathParser.Drawing.VisualNodes
             }
 
             var size = this.Node.Measure(graphics, font, out float baseline);
-            var bracketFont = GetBracketFont(graphics, font, this.LeftBracket, this.RightBracket, baseline);
+            this.MeasureInternal(graphics, font, out _, out var bracketFont, out var bracketOffset);
+            var bracketShift = new SizeF(0, bracketOffset);
 
             if (this.LeftBracket != null)
             {
-                DrawString(graphics, this.LeftBracket, bracketFont, brush, pen, topLeft);
+                DrawString(graphics, this.LeftBracket, bracketFont, brush, pen, topLeft + bracketShift);
                 topLeft.X += graphics.MeasureString(this.LeftBracket, bracketFont).Width;
             }
 
@@ -41,7 +49,7 @@ namespace MathParser.Drawing.VisualNodes
 
             if (this.RightBracket != null)
             {
-                DrawString(graphics, this.RightBracket, bracketFont, brush, pen, topLeft);
+                DrawString(graphics, this.RightBracket, bracketFont, brush, pen, topLeft + bracketShift);
             }
         }
 
@@ -52,28 +60,40 @@ namespace MathParser.Drawing.VisualNodes
                 throw new ArgumentNullException(nameof(graphics));
             }
 
+            return this.MeasureInternal(graphics, font, out baseline, out var bracketFont, out var bracketOffset);
+        }
+
+        private SizeF MeasureInternal(Graphics graphics, Font font, out float baseline, out Font bracketFont, out float bracketOffset)
+        {
             var size = this.Node.Measure(graphics, font, out baseline);
-            var bracketFont = GetBracketFont(graphics, font, this.LeftBracket, this.RightBracket, baseline);
+
+            RectangleF bothBounds;
+            using (var path = new GraphicsPath())
+            {
+                path.AddString(
+                    this.bothBrackets,
+                    font.FontFamily,
+                    (int)font.Style,
+                    graphics.DpiY * font.Size / PointsPerInch,
+                    PointF.Empty,
+                    StringFormat.GenericDefault);
+                bothBounds = path.GetBounds();
+            }
+
+            var bothSize = MeasureString(graphics, this.bothBrackets, font, out _);
+            var targetBottom = size.Height - (bothSize.Height - bothBounds.Bottom);
+            var fontScale = Math.Max((bothBounds.Top - targetBottom) / (bothBounds.Top - bothBounds.Bottom), 1);
+
+            bracketFont = fontScale <= 1 ? font : new Font(font.FontFamily, font.Size * fontScale, font.Style, font.Unit, font.GdiCharSet, font.GdiVerticalFont);
+            bracketOffset = -(fontScale * bothBounds.Top) + bothBounds.Top;
 
             var leftBracketSize = this.LeftBracket == null ? SizeF.Empty : graphics.MeasureString(this.LeftBracket, bracketFont);
             var rightBracketSize = this.RightBracket == null ? SizeF.Empty : graphics.MeasureString(this.RightBracket, bracketFont);
 
             size.Width += leftBracketSize.Width + rightBracketSize.Width;
-            size.Height = Math.Max(size.Height, Math.Max(leftBracketSize.Height, rightBracketSize.Height));
+            ////size.Height = Math.Max(size.Height, Math.Max(leftBracketSize.Height, rightBracketSize.Height) + bracketOffset);
 
             return size;
-        }
-
-        private static Font GetBracketFont(Graphics graphics, Font font, string leftBracket, string rightBracket, float contentBaseline)
-        {
-            MeasureString(graphics, leftBracket + rightBracket, font, out float normalBaseline);
-
-            if (normalBaseline == contentBaseline)
-            {
-                return font;
-            }
-
-            return new Font(font.FontFamily, font.Size * contentBaseline / normalBaseline, font.Style, font.Unit, font.GdiCharSet, font.GdiVerticalFont);
         }
     }
 }
