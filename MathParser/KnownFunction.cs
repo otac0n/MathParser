c468a74675b78649bebbb1909c80c18aa53b1858
John Gietzen <john@gietzen.us>
2025-04-03 09:43:42 -0700

    Debugging hint.

--- MathParser/KnownFunction.cs
+++ MathParser/KnownFunction.cs
@@ -1,10 +1,13 @@
 ﻿namespace MathParser
 {
+    using System.Diagnostics;
+
     /// <summary>
     /// A common handle to multiple implementations of a function.
     /// </summary>
     /// <param name="name">The name of the function.</param>
     /// <remarks>Each reference is unique, even if they share a name.</remarks>
+    [DebuggerDisplay("{Name,nq}()")]
     public class KnownFunction(string name)
     {
         /// <summary>
