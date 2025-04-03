12d804ef3ac78646225c486f6c80d3264812ae3c
John Gietzen <john@gietzen.us>
2025-04-03 09:17:53 -0700

    Refactor in progress: use public interfaces and methods to implement arithmetic operators.

--- MathParser/DefaultScope.cs
+++ MathParser/DefaultScope.cs
@@ -8,7 +8,6 @@ namespace MathParser
     using System.Linq.Expressions;
     using System.Numerics;
     using System.Reflection;
-    using System.Text.RegularExpressions;
 
     /// <summary>
     /// A temporary class for refactoring.
@@ -53,25 +52,66 @@ namespace MathParser
 
         private class WellKnownFunctionMapping : ExpressionPatternList<KnownFunction>
         {
-            public static Dictionary<string, (KnownFunction function, ExpressionType type)> Operators = new()
+            /// <summary>
+            /// <see href="https://github.com/dotnet/csharpstandard/blob/draft-v8/standard/classes.md#153106-method-names-reserved-for-operators"/> §15.3.10.6 Method names reserved for operators.
+            /// <seealso href="https://github.com/dotnet/roslyn/blob/main/src/Compilers/Core/Portable/Symbols/WellKnownMemberNames.cs">
+            /// Roslyn definition of reserved operator method names, including op_Exponent.
+            /// </seealso>
+            /// </summary>
+            public static readonly Dictionary<string, ExpressionType> OperatorMethods = new()
             {
-                ["op_UnaryNegation"] = (WellKnownFunctions.Negate, ExpressionType.Negate),
-                ["op_Addition"] = (WellKnownFunctions.Add, ExpressionType.Add),
-                ["op_Subtraction"] = (WellKnownFunctions.Subtract, ExpressionType.Subtract),
-                ["op_Multiply"] = (WellKnownFunctions.Multiply, ExpressionType.Multiply),
-                ["op_Division"] = (WellKnownFunctions.Divide, ExpressionType.Divide),
+                ["op_UnaryNegation"] = ExpressionType.Negate,
+                ["op_UnaryPlus"] = ExpressionType.UnaryPlus,
+                ["op_Addition"] = ExpressionType.Add,
+                ["op_Subtraction"] = ExpressionType.Subtract,
+                ["op_Multiply"] = ExpressionType.Multiply,
+                ["op_Division"] = ExpressionType.Divide,
+                ["op_Modulus"] = ExpressionType.Modulo,
+                ["op_Exponent"] = ExpressionType.Power,
+                ["op_CheckedAddition"] = ExpressionType.AddChecked,
+                ["op_CheckedSubtraction"] = ExpressionType.SubtractChecked,
+                ["op_CheckedMultiply"] = ExpressionType.MultiplyChecked,
+                ["op_CheckedDivision"] = ExpressionType.Divide, // DivideChecked not available.
+                ["op_OnesComplement"] = ExpressionType.OnesComplement,
+                ["op_LogicalNot"] = ExpressionType.Not,
+                ["op_BitwiseAnd"] = ExpressionType.And,
+                ["op_BitwiseOr"] = ExpressionType.Or,
+                ["op_ExclusiveOr"] = ExpressionType.ExclusiveOr,
+                ["op_Equality"] = ExpressionType.Equal,
+                ["op_Inequality"] = ExpressionType.NotEqual,
+                ["op_GreaterThan"] = ExpressionType.GreaterThan,
+                ["op_GreaterThanOrEqual"] = ExpressionType.GreaterThanOrEqual,
+                ["op_LessThan"] = ExpressionType.LessThan,
+                ["op_LessThanOrEqual"] = ExpressionType.LessThanOrEqual,
+                ["op_LeftShift"] = ExpressionType.LeftShift,
+                ["op_RightShift"] = ExpressionType.RightShift,
+            };
+
+            private static readonly Dictionary<ExpressionType, KnownFunction> Operators = new()
+            {
+                [ExpressionType.Negate] = WellKnownFunctions.Negate,
+                [ExpressionType.Add] = WellKnownFunctions.Add,
+                [ExpressionType.AddChecked] = WellKnownFunctions.Add,
+                [ExpressionType.Subtract] = WellKnownFunctions.Subtract,
+                [ExpressionType.SubtractChecked] = WellKnownFunctions.Subtract,
+                [ExpressionType.Multiply] = WellKnownFunctions.Multiply,
+                [ExpressionType.MultiplyChecked] = WellKnownFunctions.Multiply,
+                [ExpressionType.Divide] = WellKnownFunctions.Divide, // DivideChecked not available.
+                [ExpressionType.Power] = WellKnownFunctions.Pow,
             };
 
             public void Add(Type numberType)
             {
-                var methods = numberType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static).ToLookup(m => Regex.Match(m.Name, "[^.]+$").Value);
+                var methods = numberType.GetMethods(BindingFlags.Public | BindingFlags.Static).ToLookup(m => m.Name);
                 var matches = from methodGroup in methods
-                              where Operators.ContainsKey(methodGroup.Key)
-                              let known = Operators[methodGroup.Key]
+                              where OperatorMethods.ContainsKey(methodGroup.Key)
+                              let expressionType = OperatorMethods[methodGroup.Key]
+                              where Operators.ContainsKey(expressionType)
+                              let function = Operators[expressionType]
                               from method in methodGroup
                               let parameters = method.GetParameters()
                               let parameterTypes = Array.ConvertAll(parameters, p => p.ParameterType)
-                              select (expression: MakeLambda(parameterTypes, p => p.Length == 1 ? Expression.MakeUnary(known.type, p[0], null!) : Expression.MakeBinary(known.type, p[0], p[1])), known.function);
+                              select (expression: MakeLambda(parameterTypes, p => p.Length == 1 ? Expression.MakeUnary(expressionType, p[0], null!) : Expression.MakeBinary(expressionType, p[0], p[1])), function);
 
                 foreach (var (expression, function) in matches)
                 {
@@ -84,7 +124,44 @@ namespace MathParser
                     var definition = type.IsGenericType ? type.GetGenericTypeDefinition() : type;
                     var typeArgs = type.IsGenericType ? type.GetGenericArguments() : [];
 
-                    if (definition == typeof(IPowerFunctions<>))
+                    ////if (definition == typeof(IUnaryPlusOperators<,>))
+                    ////{
+                    ////    var argTypes = new[] { typeArgs[0] };
+                    ////    this.Add(MakeLambda(argTypes, p => Expression.UnaryPlus(p[0])), WellKnownFunctions.Identity);
+                    ////}
+                    ////else
+                    if (definition == typeof(IUnaryNegationOperators<,>))
+                    {
+                        var argTypes = new[] { typeArgs[0] };
+                        this.Add(MakeLambda(argTypes, p => Expression.Negate(p[0])), WellKnownFunctions.Negate);
+                        this.Add(MakeLambda(argTypes, p => Expression.NegateChecked(p[0])), WellKnownFunctions.Negate);
+                    }
+                    else if (definition == typeof(IAdditionOperators<,,>))
+                    {
+                        var argTypes = typeArgs[..^1];
+                        this.Add(MakeLambda(argTypes, p => Expression.Add(p[0], p[1])), WellKnownFunctions.Add);
+                        this.Add(MakeLambda(argTypes, p => Expression.AddChecked(p[0], p[1])), WellKnownFunctions.Add);
+                    }
+                    else if (definition == typeof(ISubtractionOperators<,,>))
+                    {
+                        var argTypes = typeArgs[..^1];
+                        this.Add(MakeLambda(argTypes, p => Expression.Subtract(p[0], p[1])), WellKnownFunctions.Subtract);
+                        this.Add(MakeLambda(argTypes, p => Expression.SubtractChecked(p[0], p[1])), WellKnownFunctions.Subtract);
+                    }
+                    else if (definition == typeof(IMultiplyOperators<,,>))
+                    {
+                        var argTypes = typeArgs[..^1];
+                        this.Add(MakeLambda(argTypes, p => Expression.Multiply(p[0], p[1])), WellKnownFunctions.Multiply);
+                        this.Add(MakeLambda(argTypes, p => Expression.MultiplyChecked(p[0], p[1])), WellKnownFunctions.Multiply);
+                    }
+                    else if (definition == typeof(IDivisionOperators<,,>))
+                    {
+                        var argTypes = typeArgs[..^1];
+                        this.Add(MakeLambda(argTypes, p => Expression.Divide(p[0], p[1])), WellKnownFunctions.Divide);
+                        var opDivideChecked = type.GetMethod("op_CheckedDivision", argTypes);
+                        this.Add(MakeLambda(argTypes, p => Expression.Divide(p[0], p[1], opDivideChecked)), WellKnownFunctions.Divide);
+                    }
+                    else if (definition == typeof(IPowerFunctions<>))
                     {
                         var argTypes = new[] { typeArgs[0], typeArgs[0] };
                         var pow = type.GetMethod(nameof(IPowerFunctions<>.Pow), argTypes);
